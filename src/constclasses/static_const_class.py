9ed22a10b3cbe21e4e8d5eba3d7a676c57e2e42d
SpectraL519 <jmusial2003@gmail.com>
2024-05-27 15:23:52 +0200

    removed the if __name__ == .__main__. block from the static_const_class.py source file

--- src/constclasses/static_const_class.py
+++ src/constclasses/static_const_class.py
@@ -13,32 +13,3 @@ def static_const_class(cls):
     StaticConstClass.__module__ = cls.__module__
 
     return StaticConstClass()
-
-
-if __name__ == "__main__":
-    print("Running main!")
-
-    @static_const_class
-    class StaticResource:
-        x: int = 1
-        s: str = "str"
-
-    print(f"{StaticResource.x = }")
-    print(f"{StaticResource.s = }")
-
-    try:
-        instance = StaticResource()
-    except Exception as err:
-        print(f"Instantiation: {err}")
-
-    try:
-        StaticResource.x = 3
-    except ConstError as err:
-        print(f"Mod x: {err}")
-
-    try:
-        StaticResource.s = "123"
-    except ConstError as err:
-        print(f"Mod s: {err}")
-
-    print("Done!")

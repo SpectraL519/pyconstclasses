6387180a5619c0a40c73a4bbc695626e0bcd7d32
Jakub Musiał <111433005+SpectraL519@users.noreply.github.com>
2024-05-27 15:26:28 +0200

    YT-PYCC-4: Add the base static_const_class decorator
    Added the `static_const_class` decorator with unit tests

--- test/test_const_class.py
+++ test/test_const_class.py
@@ -1,61 +1,44 @@
-from itertools import product
-
 import pytest
-from constclasses.ccerror import ArgumentError, ConstError
+from constclasses.ccerror import ConstError, InitializationError
 from constclasses.const_class import const_class
 
 
-class ConstClassBase:
-    x: int
-    s: str
-
-
-ConstClass = const_class(ConstClassBase)
-
-
 @const_class
-class ConstClassDecorated:
+class ConstClass:
     x: int
     s: str
 
 
-test_classes = [ConstClass, ConstClassDecorated]
-
-cls_args_combinations = [
-    pytest.param(cls, args, id=f"cls={cls.__name__}, args={args}")
-    for cls, args in product(test_classes, [(), (1,), (1, 2, 3)])
-]
-
-
-@pytest.mark.parametrize("const_cls, init_args", cls_args_combinations)
-def test_const_class_initialization_with_invalid_number_of_arguments(
-    const_cls, init_args: list
-):
-    with pytest.raises(ArgumentError) as err:
-        _ = const_cls(init_args)
+@pytest.mark.parametrize(
+    "init_args",
+    [pytest.param(args, id=f", args={args}") for args in [(), (1,), (1, 2, 3)]],
+)
+def test_const_class_initialization_with_invalid_number_of_arguments(init_args: list):
+    with pytest.raises(InitializationError) as err:
+        _ = ConstClass(init_args)
 
     err_msg = str(err.value)
     assert err_msg.startswith("Invalid number of arguments")
 
 
-@pytest.mark.parametrize(
-    "const_cls",
-    [pytest.param(cls, id=f"cls={cls.__name__}") for cls in test_classes],
-)
-def test_const_class_member_modification(const_cls):
+def test_const_class_member_modification():
     x1, s1 = 1, "str1"
     x2, s2 = 2, "str2"
 
-    const_instance = const_cls(x1, s1)
+    const_instance = ConstClass(x1, s1)
 
     with pytest.raises(ConstError) as err:
         const_instance.x = x2
 
+    build_err_msg = (
+        lambda member: f"Cannot modify const attribute `{member}` of class `{ConstClass.__name__}`"
+    )
+
     err_msg = str(err.value)
-    assert err_msg == f"Cannot modify const attribute `x` of class `{const_cls.__name__}`"
+    assert err_msg == build_err_msg("x")
 
     with pytest.raises(ConstError) as err:
         const_instance.s = s2
 
     err_msg = str(err.value)
-    assert err_msg == f"Cannot modify const attribute `s` of class `{const_cls.__name__}`"
+    assert err_msg == build_err_msg("s")

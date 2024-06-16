ffd5805da15078b99332651eeaa4cc8db4f198b8
Jakub Musiał <111433005+SpectraL519@users.noreply.github.com>
2024-06-16 12:21:18 +0200

    YT-PYCC-5: Add mutable_instance function
    Added:
    - the `mutable_instance` function for the `static_const_class` decorated classes
    - a simple formatting script

--- src/constclasses/ccerror.py
+++ src/constclasses/ccerror.py
@@ -3,13 +3,13 @@ class InitializationError(Exception):
         super().__init__(message)
 
     @staticmethod
-    def invalid_number_of_arguments_error(nexpected: int, nactual: int):
+    def invalid_number_of_arguments(nexpected: int, nactual: int):
         return InitializationError(
             f"Invalid number of arguments: expected {nexpected} - got {nactual}"
         )
 
 
 class ConstError(Exception):
-    def __init__(self, cls_name, attribute):
+    def __init__(self, cls_name: str, attribute: str):
         message = f"Cannot modify const attribute `{attribute}` of class `{cls_name}`"
         super().__init__(message)
--- src/constclasses/static_const_class.py
+++ src/constclasses/static_const_class.py
@@ -1,15 +1,45 @@
-from .ccerror import ConstError, InitializationError
+from .ccerror import ConstError
 
 
 def static_const_class(cls):
     class StaticConstClass(cls):
-        def __init__(self, *args):
-            pass
+        def __init__(self, *args, **kwargs):
+            self.__dict__["_initialized"] = False
+            super(StaticConstClass, self).__init__(*args, **kwargs)
 
-        def __setattr__(self, attr_name: str, _) -> None:
-            raise ConstError(cls.__name__, attr_name)
+        def __setattr__(self, attr_name: str, value) -> None:
+            if self._initialized:
+                raise ConstError(cls.__name__, attr_name)
+            self.__dict__[attr_name] = value
 
     StaticConstClass.__name__ = cls.__name__
     StaticConstClass.__module__ = cls.__module__
+    StaticConstClass.__annotations__ = cls.__annotations__
 
-    return StaticConstClass()
+    cls_vars = vars(cls)
+    instance = StaticConstClass()
+    for attr_name, attr_type in StaticConstClass.__annotations__.items():
+        setattr(instance, attr_name, attr_type(cls_vars[attr_name]))
+    instance._initialized = True
+
+    return instance
+
+
+def mutable_instance(static_const_cls_instance):
+    static_const_cls = static_const_cls_instance.__class__
+    print(static_const_cls_instance.__dict__)
+
+    class MutableClass(static_const_cls):
+        def __init__(self, *args, **kwargs):
+            super(MutableClass, self).__init__(*args, **kwargs)
+
+            for attr_name, attr_value in static_const_cls_instance.__dict__.items():
+                self.__dict__[attr_name] = attr_value
+
+        def __setattr__(self, attr_name, value):
+            self.__dict__[attr_name] = value
+
+    MutableClass.__name__ = f"mutable_{static_const_cls.__name__}"
+    MutableClass.__module__ = static_const_cls.__module__
+
+    return MutableClass()

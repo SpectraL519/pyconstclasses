f922718cf4853cf61d6a51feddd982d0b4a8509b
Jakub Musiał <111433005+SpectraL519@users.noreply.github.com>
2024-06-16 12:21:18 +0200

    YT-PYCC-5: Add mutable_instance function
    Added:
    - the `mutable_instance` function for the `static_const_class` decorated classes
    - a simple formatting script

--- test/test_static_const_class.py
+++ test/test_static_const_class.py
@@ -1,29 +1,28 @@
 import pytest
 from constclasses.ccerror import ConstError, InitializationError
-from constclasses.static_const_class import static_const_class
+from constclasses.static_const_class import mutable_instance, static_const_class
+
+X1, S1 = 1, "str1"
+X2, S2 = 2, "str2"
 
 
 @static_const_class
 class StaticConstResource:
-    x: int = 1
-    s: str = "str"
+    x: int = X1
+    s: str = S1
 
 
 def test_static_const_class_initialization_error():
-    x2, y2 = 2, "str2"
-
     with pytest.raises(TypeError) as err:
-        _ = StaticConstResource(x2, y2)
+        _ = StaticConstResource(X2, S2)
 
     err_msg = str(err.value)
     assert err_msg == "'StaticConstResource' object is not callable"
 
 
 def test_static_const_class_member_modification():
-    x2, s2 = 2, "str2"
-
     with pytest.raises(ConstError) as err:
-        StaticConstResource.x = x2
+        StaticConstResource.x = X2
 
     build_err_msg = (
         lambda member: f"Cannot modify const attribute `{member}` of class `StaticConstResource`"
@@ -33,7 +32,24 @@ def test_static_const_class_member_modification():
     assert err_msg == build_err_msg("x")
 
     with pytest.raises(ConstError) as err:
-        StaticConstResource.s = s2
+        StaticConstResource.s = S2
 
     err_msg = str(err.value)
     assert err_msg == build_err_msg("s")
+
+
+def test_mutable_instance_of_static_const_class():
+    mut_instance = mutable_instance(StaticConstResource)
+
+    assert mut_instance.x == X1
+    assert mut_instance.s == S1
+
+    mut_instance.x = X2
+    assert mut_instance.x == X2
+
+    mut_instance.s = S2
+    assert mut_instance.s == S2
+
+
+if __name__ == "__main__":
+    mut_instance = mutable_instance(StaticConstResource)
